c8a5b226cd53491f39f8bb713e407ad23cd73fa7
Aditya Chatterjee <aditianhacker@gmail.com>
2017-05-27 17:16:16 +0530

    Get locations of all your fans :whale:

--- code/main.py
+++ code/main.py
@@ -17,15 +17,35 @@ def follower_list(username):
         count += 1
     return stargazers
 
-def get_user_profile(username):
+def get_user_data(username):
     url_ = "https://api.github.com/users/{}?access_token={}".format(username, access_token)
     r = urllib.request.urlopen(url_).read()
     result = json.loads(r)
     return result['location'], result['id']
+
+def get_locations_of_fans(stargazers):
+
+    data = {'users': []}
+
+    for user in stargazers:
+        user_country, user_id = get_user_data(user)
+
+        if user_country == "N/A":
+            pass
+        else:
+            user_country = geocoder.google(user_country).country_long
+            if user_country:
+                user_ = {"user": user_id, "country": user_country}
+                data["users"].append(user_)
+                print ("A fan from " + user_country)
+
+    return data
 
 if __name__ == "__main__":
     import sys
     
     username = str(sys.argv[1])  
     stargazers = follower_list(username)
+    location_data = get_locations_of_fans(stargazers)
+
     print ("completed.")
