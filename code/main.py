bd2d5b188584748b6ca99a4760f65cd7b5652de9
Aditya Chatterjee <aditianhacker@gmail.com>
2017-05-28 18:47:14 +0530

    data of @adichat :family_woman_boy:

--- code/main.py
+++ code/main.py
@@ -7,7 +7,7 @@ access_token = '' # get your own access token for GitHub API
 
 def follower_list(username):
     page = 1
-    stargazers = []
+    fans = []
     while True:
         url = "https://api.github.com/users/{}/followers?page={}&per_page=100&access_token={}".format(username, page, access_token)
         r = urllib.request.urlopen(url).read()
@@ -18,9 +18,9 @@ def follower_list(username):
         page += 1
         count = 0    
         while count<followers:
-            stargazers.append(result[count]["login"])
+            fans.append(result[count]["login"])
             count += 1
-    return stargazers
+    return fans
 
 def get_user_data(username):
     url_ = "https://api.github.com/users/{}?access_token={}".format(username, access_token)
@@ -28,11 +28,11 @@ def get_user_data(username):
     result = json.loads(r)
     return result['location'], result['id']
 
-def get_locations_of_fans(stargazers):
+def get_locations_of_fans(fans):
 
     data = {'users': []}
 
-    for user in stargazers:
+    for user in fans:
         user_country, user_id = get_user_data(user)
 
         if user_country == "N/A":
@@ -51,25 +51,25 @@ def write_in_js_file(location_data):
     month = str(datetime.datetime.now().month)
     day = str(datetime.datetime.now().day)
 
-    with open("js/stargazers.js", "w") as js_file:
+    with open("js/fans.js", "w") as js_file:
         js_file.write("var data = { users:")
         js_file.write(str(location_data["users"]))
         js_file.write(", created_at: new Date(%s, %s, %s) };" % (year, month, day))
 
     js_file.close()
 
-def store_follower_list(stargazers, location):
+def store_follower_list(fans, location):
     with open(location, 'w') as outfile:
-        json.dump(stargazers, outfile, indent=4)
+        json.dump(fans, outfile, indent=4)
     outfile.close()
 
 if __name__ == "__main__":
     import sys
     
     username = str(sys.argv[1])  
-    stargazers = follower_list(username)
-    store_follower_list(stargazers, 'data/stargazers.json')
-    location_data = get_locations_of_fans(stargazers)
+    fans = follower_list(username)
+    store_follower_list(fans, 'data/fans.json')
+    location_data = get_locations_of_fans(fans)
     write_in_js_file(location_data)
 
     print ("completed.")
